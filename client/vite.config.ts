473c7f50ad114379430f1b5f20b15aa7c51ac1de
Cody Christ <cchrist1210@gmail.com>
2024-02-14 11:52:16 -0800

    Enriched TypeScript + Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import svgr from "vite-plugin-svgr";
+import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+const host = process.env.HOST || true;
+const port = +(process.env.PORT || 5173);
+const projectRoot = resolve(__dirname, "src");
+
+export default defineConfig(() => {
+  return {
+    root: "./src",
+    server: { host, port },
+    preview: { host, port },
+    plugins: [
+      svgr(),
+      react(),
+      AutoImport({
+        imports: ["vitest"],
+        dts: true, // generate Typescript definitions
+      }),
+      ImportMetaEnvPlugin.vite({
+        example: ".env.example",
+        env: ".env"
+      }),
+    ],
+    build: {
+      outDir: "../build",
+      sourcemap: true,
+    },
+    resolve: {
+      alias: {
+        "@root": projectRoot,
+        "~": projectRoot,
+      },
+    },
+  };
 });
